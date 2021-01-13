e21cbe27600016c14418073b3abc12117967f521
spieta <spieta2@gmail.com>
2021-01-14 00:24:01 +0100

    Add cart database operations

--- topszop/server/database_operations/cart.py
+++ topszop/server/database_operations/cart.py
@@ -1,14 +1,34 @@
-def get_all_product_from_cart():
-    pass
+from server.models import Cart, Cart_Product, Product
 
-def set_amount_of_product_in_cart():
-    pass
+def get_all_product_from_cart(cart_id=1):
+    """Returns list of all products and their amounts in the cart.
+    amounts - list of ints
+    products - list of models.Product
+    """
 
-def add_product_to_cart():
-    pass
+    products = []
+    amounts = []
 
-def get_product_from_search():
-    pass
+    cart_products = Cart_Product.objects.filter(cart_id__exact=cart_id)
+    for cart_product in cart_products:
+        products.append(Product.objects.get(id__exact=cart_product.product_id))
+        amounts.append(cart_product.amount)
 
-def remove_product_from_cart():
-    pass
+    return products, amounts
+
+def set_amount_of_product_in_cart(amount, product_id, cart_id=1):
+    """Find Cart by cart_id, find Product by product_id and set its amount to amount"""
+
+    Cart_Product.objects.filter(cart_id__exact=cart_id) \
+                        .filter(product_id__exact=product_id) \
+                        .update(amount=amount)
+
+def add_product_to_cart(amount, product_id, cart_id=1):
+    """Add amount of new Product with product_id to Cart with cart_id"""
+
+    Cart_Product.objects.create(cart_id=cart_id, product_id=product_id, amount=amount)
+
+def remove_product_from_cart(product_id, cart_id=1):
+    """Remove Cart_Product with matching product_id and cart_id <=> remove Product from Cart"""
+
+    Cart_Product.objects.filter(cart_id__exact=cart_id).filter(product_id__exact=product_id).delete()
--- topszop/server/database_operations/product.py
+++ topszop/server/database_operations/product.py
@@ -1,3 +1,5 @@
+from server.models import Product
+
 def get_all_products():
     pass
 
@@ -12,3 +14,8 @@ def get_duplicate():
 
 def remove_product():
     pass
+
+def get_product_from_search(querry):
+    """Find all Products which names match the querry. Returns a list of models.Product"""
+
+    return list(Product.objects.filter(name__icontains=querry))
